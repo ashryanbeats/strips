c51ac8da8b144a9fdaac552415f9aba668774504
Ash Ryan <ash@ashryanbeats.com>
2015-02-03 11:18:22 +0900

    Content of footer now displaying correctly.

--- footer.php
+++ footer.php
@@ -10,20 +10,20 @@
 				<div id="footer" class="col-sm-12">
 					<!-- footer widget -->
 					<?php if (is_active_sidebar('footer2')) : ?>
-						<div class="col-sm-6" id="footer-widget-1">
+						<div class="col-sm-5 col-md-offset-1" id="footer-widget-1">
 					<?php else : ?>
-						<div class="col-md-12">
+						<div class="col-md-10 col-md-offset-1">
 					<?php endif ?>	
 						<?php dynamic_sidebar('footer1') ?>
 						</div>
 						
 					<?php if (is_active_sidebar('footer2')) : ?>
-						<div class="col-sm-6" id="footer-widget-2">
+						<div class="col-sm-5" id="footer-widget-2">
 							<?php dynamic_sidebar('footer2') ?>
 						</div>
 					<?php endif ?>
 					
-						<div class="col-sm-12" id="copy">
+						<div class="col-md-10 col-md-offset-1" id="copy">
 							<small>
 								<p>Strips theme for WordPress by Ash Ryan, currently in development.</p>
 								<p><?php echo '©2010 – '.date("Y"). ' <a href="http://ashryanbeats.com">Ash Ryan Beats</a>' ?></p>
