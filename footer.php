e709cd3d4de337fc361c08cb1aaa63533b684323
Ash Ryan <ash@ashryanbeats.com>
2015-02-14 15:48:06 -0600

    Added function to functions.php to handle the_excerpt() so that HTML tags show in excerpts. Removed old functions related to the_excerpt().

--- footer.php
+++ footer.php
@@ -10,9 +10,9 @@
 				<div id="footer" class="col-sm-12">
 					<!-- footer widget -->
 					<?php if (is_active_sidebar('footer2')) : ?>
-						<div class="col-sm-5 col-md-offset-1" id="footer-widget-1">
+						<div class="col-sm-5 col-sm-offset-1" id="footer-widget-1">
 					<?php else : ?>
-						<div class="col-md-10 col-md-offset-1">
+						<div class="col-sm-10 col-sm-offset-1">
 					<?php endif ?>	
 						<?php dynamic_sidebar('footer1') ?>
 						</div>
@@ -23,7 +23,7 @@
 						</div>
 					<?php endif ?>
 					
-						<div class="col-md-10 col-md-offset-1" id="copy">
+						<div class="col-sm-10 col-sm-offset-1" id="copy">
 							<small>
 								<p>Strips theme for WordPress by Ash Ryan, currently in development.</p>
 								<p><?php echo '©2010 – '.date("Y"). ' <a href="http://ashryanbeats.com">Ash Ryan Beats</a>' ?></p>
--- functions.php
+++ functions.php
@@ -58,24 +58,7 @@
         'page-thumb' => __('Page Thumb'),
 		) );
 	}
-	
-	//adds the "... Read more" link on the end of the_excerpt()
-	function new_excerpt_more( $more ) {
-		return '... <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read more</a>';
-	}
-	add_filter('excerpt_more', 'new_excerpt_more');
-	
-	//changes the length of the excerpt. the units are words. what about characters in Japanese? would an if statement based on language work? 
-	function custom_excerpt_length( $length ) {
-		return 40;
-	}
-	add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
-	
-	//adds a class (.excerpt) to the_excerpt()
-	add_filter( "the_excerpt", "add_class_to_excerpt" );
-	function add_class_to_excerpt( $excerpt ) {
-    	return str_replace('<p', '<p class="excerpt"', $excerpt);
-	}
+
 	
 	function strips_customize_register( $wp_customize ) {
 		$wp_customize->add_setting( 'header_textcolor' , array(
@@ -116,5 +99,77 @@
 		return $headline;
 	}
 	add_filter( 'jetpack_relatedposts_filter_headline', 'jetpackme_related_posts_headline' );*/
+	
+	
+	
+	// this function and the if statment below cover the_excerpt()
+	function wpse_allowedtags() {
+    // Add custom tags to this string
+        return '<script>,<style>,<br>,<em>,<i>,<ul>,<ol>,<li>,<a>,<p>,<img>,<video>,<audio>,<code>,<pre>,<blockquote>'; 
+    }
+
+	if ( ! function_exists( 'wpse_custom_wp_trim_excerpt' ) ) : 
+	
+	    function wpse_custom_wp_trim_excerpt($wpse_excerpt) {
+	    global $post;
+	    $raw_excerpt = $wpse_excerpt;
+	        if ( '' == $wpse_excerpt ) {
+	
+	            $wpse_excerpt = get_the_content('');
+	            $wpse_excerpt = strip_shortcodes( $wpse_excerpt );
+	            $wpse_excerpt = apply_filters('the_content', $wpse_excerpt);
+	            $wpse_excerpt = str_replace(']]>', ']]&gt;', $wpse_excerpt);
+	            $wpse_excerpt = strip_tags($wpse_excerpt, wpse_allowedtags()); /*IF you need to allow just certain tags. Delete if all tags are allowed */
+	
+	            //Set the excerpt word count and only break after sentence is complete.
+	                $excerpt_word_count = 40;
+	                $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count); 
+	                $tokens = array();
+	                $excerptOutput = '';
+	                $count = 0;
+	
+	                // Divide the string into tokens; HTML tags, or words, followed by any whitespace
+	                preg_match_all('/(<[^>]+>|[^<>\s]+)\s*/u', $wpse_excerpt, $tokens);
+	
+	                foreach ($tokens[0] as $token) { 
+	
+	                    if ($count >= $excerpt_word_count && preg_match('/[\?\.\!]\s*$/uS', $token)) { 
+	                    // Limit reached, continue until , ; ? . or ! occur at the end
+	                        $excerptOutput .= trim($token);
+	                        break;
+	                    }
+	
+	                    // Add words to complete sentence
+	                    $count++;
+	
+	                    // Append what's left of the token
+	                    $excerptOutput .= $token;
+	                }
+	
+	            $wpse_excerpt = trim(force_balance_tags($excerptOutput));
+	
+	                
+		            $excerpt_end = '<a class="read-more" alt= "' . get_the_title() . '" href="'. esc_url( get_permalink() ) . '">' . sprintf(__( 'Read more >' ), get_the_title()) . '</a>'; 
+	                $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end); 
+	
+	                //$pos = strrpos($wpse_excerpt, '</');
+	                //if ($pos !== false)
+	                // Inside last HTML tag
+	                //$wpse_excerpt = substr_replace($wpse_excerpt, $excerpt_end, $pos, 0); /* Add read more next to last word */
+	                //else
+	                // After the content
+	                
+		            $wpse_excerpt .= $excerpt_end; /*Add read more in new paragraph */
+	
+	            return $wpse_excerpt;   
+	
+	        }
+	        return apply_filters('wpse_custom_wp_trim_excerpt', $wpse_excerpt, $raw_excerpt);
+	    }
+	
+	endif; 
+	
+	remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+	add_filter('get_the_excerpt', 'wpse_custom_wp_trim_excerpt'); 
 
 ?>
