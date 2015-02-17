181b47dfeb5c321508bf13236514d91da471ca7b
Ash Ryan <ash@ashryanbeats.com>
2015-02-17 16:49:22 -0600

    Removed meta description in order to play well with SEO plugins.

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="<?php bloginfo('charset') ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="<?php bloginfo('description') ?>"/>
 	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>"/>
 	<title><?php bloginfo('name')?> <?php wp_title() ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri() ?>" media="all">
